5c6b4695821eeef7b3eebaf54d73f0d361d98f62
feiy <feiy@nvidia.com>
2023-12-24 12:30:55 +0800

    Http async implement from C++ side

--- GUITest/http_get.js
+++ GUITest/http_get.js
@@ -1,19 +1,6 @@
-function httpGetAsync(url, is_text) {
-    return new Promise((resolve, reject) => {
-        http.getAsync(url, is_text, (suc, data) => {
-            if (suc) {
-                resolve(data);
-            }
-            else {
-                reject();
-            }
-        });
-    });
-}
-
 (async ()=>
 {
-    text = await httpGetAsync("https://www.gutenberg.org/cache/epub/1065/pg1065.txt", true);
+    text = await http.getAsync("https://www.gutenberg.org/cache/epub/1065/pg1065.txt", true);
     print(text);        
     
 })()
--- GUITest/script_window.js
+++ GUITest/script_window.js
@@ -1,24 +1,8 @@
-function httpGetAsync(url, is_text)
-{
-    return new Promise((resolve, reject) => {
-        http.getAsync(url, is_text, (suc, data)=>
-        {
-            if (suc) {
-                resolve(data);
-            }
-            else
-            {
-                reject();
-            }
-        });
-    });
-}
-
 let input_text = new InputText("##URL", 256, "https://www.gutenberg.org/cache/epub/1065/pg1065.txt");
 
 let button_get = new Button("Get!");
 button_get.onClick = async ()=>{
-    let text = await httpGetAsync(input_text.text, true);
+    let text = await http.getAsync(input_text.text, true);
     print(`Text From: ${input_text.text}`);
     print(text);
 };
