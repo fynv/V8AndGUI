0599f29b3703723672fd2e5544efe1a9f671028a
feiy <feiy@nvidia.com>
2023-12-29 09:54:26 +0800

    bug fix

--- GUITest/script_window.js
+++ GUITest/script_window.js
@@ -1,9 +1,10 @@
 let input_text = new InputText("##URL", 256, "https://www.gutenberg.org/cache/epub/1065/pg1065.txt");
 
 let button_get = new Button("Get!");
-button_get.onClick = async ()=>{
-    let text = await http.getAsync(input_text.text, true);
-    print(`Text From: ${input_text.text}`);
+button_get.onClick = async () => {
+    let url = input_text.text;
+    let text = await http.getAsync(url, true);
+    print(`Text From: ${url}`);
     print(text);
 };
 
